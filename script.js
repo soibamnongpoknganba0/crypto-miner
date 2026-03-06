d00eccbf98f28baa6d3e50844740c699bf143570
soibamnongpoknganba0 <soibamnongpoknganba0@gmail.com>
2026-03-06 15:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let mining = false
+let hashes = 0
+
+function startMining(){
+mining = true
+document.getElementById("status").innerText = "Running"
+mine()
+}
+
+function stopMining(){
+mining = false
+document.getElementById("status").innerText = "Stopped"
+}
+
+function mine(){
+if(!mining) return
+
+hashes++
+
+document.getElementById("hashes").innerText = hashes
+
+setTimeout(mine,10)
+}
